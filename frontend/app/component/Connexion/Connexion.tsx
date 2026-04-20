847523760bc2092a9888e75513a7063f07d74877
Riner Alexandre <lloyd.officiel@hotmail.fr>
2026-04-20 15:16:26 +0200

    test8

--- frontend/app/component/Connexion/Connexion.tsx
+++ frontend/app/component/Connexion/Connexion.tsx
@@ -44,7 +44,7 @@ export default function Connexion() {
     };
 
     return (
-        <div className="bg-white backdrop-blur-md p-6 sm:p-10 rounded-xl shadow-2xl w-full max-w-md mx-4 sm:mx-0">
+        <div className="bg-white backdrop-blur-md p-6 sm:p-10 rounded-xl shadow-2xl w-full max-w-md">
             <div className="flex justify-center gap-1">
                 <Image src="/images/NEWSFOUNDRY.svg" alt="Logo" width={150} height={150} />
                 <Image src="/images/Union-3.svg" alt="Logo" width={20} height={20} />
--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -11,8 +11,8 @@ export default function LoginPage() {
         className="absolute top-0 left-0 w-full h-full object-cover -z-10"
         priority
       />
-      <div className="flex justify-center items-center h-screen z-10 relative">
-        <main className="">
+      <div className="flex justify-center items-center h-screen z-10 relative px-4 w-full">
+        <main className="w-full max-w-md">
           <Connexion />
         </main>
       </div>
